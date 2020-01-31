97137921cf62a49a173784ae6c1ec4ba4a29fb5f
Silzhar <luis.ruizfer@gmail.com>
2020-01-31 20:11:38 +0100

    clase upgrade 30/02/2020

--- ejercicios_php/04-class_fichero/FIchero.php
+++ ejercicios_php/04-class_fichero/FIchero.php
@@ -1,17 +1,46 @@
 <?php
 
-$files = fopen("/fichero.txt", "a+");
-
 class Fichero{
 
-    $new_file = $files;
+    protected $fichero;
 
-    function Delete()
-    {
-        
+    public function __contructor($fichero){
+        $this->fichero = $fichero;
+
+        if (!file_exists($fichero)){
+            $peliculas = [
+                [
+                    "nombre"=> "Los Vengadores",
+                    "año"=>2015
+                ],
+                ["nombre"=> "Matrix", "año"=> 1999]
+            ];
+
+            $peliculaJSON = json_encode($peliculas);
+
+            file_put_contents($fichero,$peliculaJSON);
+        }
     }
-
-
+    public function copiar($ficheroDestino){
+        if (!copy($this->fichero, $ficheroDestino)){
+            echo "Error al copiar el archivo";
+        }
+        echo "copia de $this->fichero a $ficheroDestino<br>";
+    }
+    public function mover($ficheroDestino){
+        rename($this->fichero, $ficheroDestino);
+        echo "renombre de $this->fichero a $ficheroDestino<br>";
+    }
+    public function leer(){
+        return file_get_contents($this->fichero);
+    }
+    public function borrar(){
+        unlink($this->fichero);
+        echo "borro $this->fichero";
+    }
+    public function fechaModificacion(){
+        return filemtime($this->fichero);
+    }
 }
 
 ?>
