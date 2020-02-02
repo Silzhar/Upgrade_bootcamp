c271b9662721a22eb96a11d26a9cb0fe7f52a200
Silzhar <luis.ruizfer@gmail.com>
2020-02-02 20:24:46 +0100

    repair index.php(llamada)

--- ejercicios_php/06-serie/index.php
+++ ejercicios_php/06-serie/index.php
@@ -3,7 +3,7 @@
 require_once ("Serie.php");
 require_once ("Temporadas.php");
 
-$temporada = new Temporada("temporada1.json");
+$temporada = new Temporadas("temporada1.json");
 
 $visualizador = $temporada->ver();
 echo $visualizador;
