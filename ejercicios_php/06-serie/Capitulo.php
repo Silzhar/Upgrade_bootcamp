13c5c62175c4e91819b4e79852ae9334beccc8e3
Silzhar <luis.ruizfer@gmail.com>
2020-02-04 20:33:12 +0100

    update serie / add capitulo.php

---
+++ ejercicios_php/06-serie/Capitulo.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Capitulo{
+
+    private $capitulo;
+    private $numCapitulo;
+    private $valoracion;
+
+    public function __construct(string $titulo, int $numCapitulo, int $valoracion){
+
+        $this->capitulo = $capitulo;
+        $this->numCapitulo = $numCapitulo;
+        $this->valoracion = $valoracion;
+    }
+}
+
+?>
