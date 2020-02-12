bd022913007780b8b3675c5337de4db4d98e6812
Silzhar <luis.ruizfer@gmail.com>
2020-02-12 12:37:01 +0100

    ejercicios varios

---
+++ mix-ejercicios/calculadora/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CALCULADORA</title>
+</head>
+<body>
+    <div>
+        <form action="calculadora">
+            <input type="number" placeholder="numero1">
+            <input type="number" placeholder="numerero2">
+        </form>
+    </div>
+    <?php
+
+    $numero1 = x;
+    $numero2 = y;
+
+    $suma = $numero1 + $numero2;
+
+    $resta = $numero1 - $numero2;
+
+    $producto = $numero1 * $numero2;
+
+    $division = $numero1 / $nuemro2;
+
+    ?>
+</body>
+</html>
