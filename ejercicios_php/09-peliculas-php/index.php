b187214d548b9a857a542801473c83e12cc1875a
Silzhar <luis.ruizfer@gmail.com>
2020-02-14 10:03:49 +0100

    peliculas-php

---
+++ ejercicios_php/09-peliculas-php/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once ("Fichero.php");
+
+$peliculas = new Fichero("peliculas.json");
+
+$peliculas->copiar("copia_peliculas.json");
+
+$contenidoPeliculas = $peliculas->leer();
+echo $contenidoPeliculas;
+
+$peliculas->borrar();
+
+$copiaPeliculas = new Fichero("copia_peliculas.json");
+
+echo "La fecha de modificación es: ".$copiaPeliculas->fechaModificacion();
+
+$copiaPeliculas->mover("peliculas.json");
