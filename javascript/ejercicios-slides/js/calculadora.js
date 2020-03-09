61186595e72dc82ab8e0c052e24766acf921c901
Silzhar <luis.ruizfer@gmail.com>
2020-03-09 18:46:19 +0100

    solucion division / calculadora JS

--- javascript/ejercicios-slides/js/calculadora.js
+++ javascript/ejercicios-slides/js/calculadora.js
@@ -1,75 +1,87 @@
 
-var textArea = document.getElementById("resultado").value
+var textArea = parseFloat(document.getElementById("resultado").value);
+var variables = [];
+var salida = 0;
+var operadores = ["+","-","/","*"];
 
-function cero(){
-    if(textArea == ""){
-        document.getElementById("resultado").innerHTML = "0";
+function numero(entrada){
+    if(textArea === 0 ){
+        document.getElementById("resultado").value = entrada ;
+        
         console.log('if',textArea);
     }else{
-        var incluir = textArea.value + "0";
-        document.getElementById("resultado").innerHTML = incluir;
-        console.log('else',textArea);
-
+        salida =  document.getElementById("resultado").value += entrada;
+        
     }
     
 }
 
-function uno(){
-    document.getElementById("resultado").innerHTML = "1";
+function dividir(textArea){
+    textArea = parseFloat(document.getElementById("resultado").value);
+    variables.push(textArea);
+    document.getElementById("resultado").value = "";
+    
+    if(variables.length == 2){
+        var total = variables[0] / variables[1];
+        document.getElementById("resultado").value = total;
+        console.log('division ',total);
+        variables.length = 0;
+        
+    }else if(variables.length == 1){
+
+    }
+    else{
+        document.getElementById("resultado").value = "";
+        total = (variables.length -2) / (variables.length -1);
+        document.getElementById("resultado").value = total;
+
+    }
 }
 
-function dos(){
-    document.getElementById("resultado").innerHTML = "2";
+
+function multiplicar(textArea){
+    textArea = parseFloat(document.getElementById("resultado").value);
+    variables.push(textArea);
+    document.getElementById("resultado").value = "";
+    var total = variables[0] * variables[1];
+    console.log('multiplicacion ',total);
 }
 
-function tres(){
-    document.getElementById("resultado").innerHTML = "3";
+
+function sumar(textArea){
+    textArea = parseFloat(document.getElementById("resultado").value);
+    variables.push(textArea);
+    document.getElementById("resultado").value = "";
+    var total = variables[0] + variables[1];
+    console.log('suma ',total);
+    document.getElementById("resultado") = total;
+    }
+
+function restar(textArea){
+    textArea = parseFloat(document.getElementById("resultado").value);
+    variables.push(textArea);
+    document.getElementById("resultado").value = "";
+    var total = variables[0] - variables[1];
+    console.log('resta ',total);
+    document.getElementById("resultado") = total;
 }
 
-function cuatro(){
-    document.getElementById("resultado").innerHTML = "4";
+
+
+function igual(textArea, textArea2){
+    textArea2 = parseFloat(document.getElementById("resultado").value);
+
+    if(textArea2.get(0).selectionStart == "/"){
+    
+        var operando = parseFloat(document.getElementById("resultado").value);
+        console.log("array operando", operando);
+        console.log('textarea 2',textArea2);
+
+    }
+    document.getElementById("resultado").value = "=";
 }
 
-function cinco(){
-    document.getElementById("resultado").innerHTML = "5";
-}
-
-function seis(){
-    document.getElementById("resultado").innerHTML = "6";
-}
-
-function siete(){
-    document.getElementById("resultado").innerHTML = "7";
-}
-
-function ocho(){
-    document.getElementById("resultado").innerHTML = "8";
-}
-
-function nueve(){
-    document.getElementById("resultado").innerHTML = "9";
-}
-
-function dividir(){
-    document.getElementById("resultado").innerHTML = "/";
-}
-
-function multiplicar(){
-    document.getElementById("resultado").innerHTML = "*";
-}
-
-function sumar(){
-    document.getElementById("resultado").innerHTML = "+";
-}
-
-function restar(){
-    document.getElementById("resultado").innerHTML = "-";
-}
-
-function igual(){
-    document.getElementById("resultado").innerHTML = "=";
-}
-
-function limpiar(){
-    document.getElementById("resultado").innerHTML = " ";
+function limpiar(variables){
+    document.getElementById("resultado").value = " ";
+    variables.length = 0;
 }
