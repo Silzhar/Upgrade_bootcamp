f2ebc6c7ee2952400063d7c6c4e70a0a046d3e46
Silzhar <luis.ruizfer@gmail.com>
2020-03-12 23:23:09 +0100

    work in 09 slides JS / part 3

--- javascript/ejercicios-slides/js/caculadora-2.js
+++ javascript/ejercicios-slides/js/caculadora-2.js
@@ -1,8 +1,11 @@
 var textArea = parseFloat(document.getElementById("resultado").value);
 var variables = [];
 var procesos = [];
+
 var totalSuma = 0;
 var operadorUno = 0;
+
+
 
 
 function numero(entrada){
@@ -20,39 +23,81 @@ function numero(entrada){
     variables.push(operadorUno);
     procesos.push("+");
     document.getElementById("resultado").value = " ";
-    // if (variables.length > 2){  
-    //     variables.forEach(function(operador){
-    //         totalSuma += operador;
-    //         return totalSuma;
-    //     });   
-    //     }
-    }
-    
+}
+
+function restar(){
+    variables.push(operadorUno);
+    procesos.push("-");
+    document.getElementById("resultado").value = " ";
+}
+
+function multiplicar(){
+    variables.push(operadorUno);
+    procesos.push("*");
+    document.getElementById("resultado").value = " ";
+}
+
+function dividir(){
+    variables.push(operadorUno);
+    procesos.push("/");
+    document.getElementById("resultado").value = " ";
+}
+
 
 function igual(){
-    var suma =  procesos[0] == "+";
+    var suma =  procesos[0] === "+";
+    var resta =  procesos[0] === "-";
+    var division =  procesos[0] === "/";
+    var multiplicacion =  procesos[0] === "*";
+    console.log("suma ",suma);
+    console.log("resta ",resta);
+    console.log("div ",division);
+    console.log("* ",multiplicacion);
 
     var operadorDos = parseFloat(document.getElementById("resultado").value);
     variables.push(operadorDos);
 
-    switch(procesos.length > 1){
-        case suma:
-            console.log("switch suma ");
-            if (variables.length > 2){  
-                variables.forEach(function(operador){
-                    totalSuma += operador;
-                    return totalSuma;
-                });   
-            }
-            break;
-        }     
-        // default:
-        //     console.log("no entra en switch - case");
+
+    if (suma == true){
+        console.log("suma ");
     
+        variables.forEach(function(operador){
+            totalSuma += operador;
+            document.getElementById("resultado").value = totalSuma;
+                
+            });
+    }
 
+    else if (resta == true){
+        console.log(" resta ");
+        totalSuma = variables.reduce((primerNumero, segundoNumero) => primerNumero - segundoNumero);
+        document.getElementById("resultado").value = totalSuma;
+    }
+        
+    else if (division == true){
+        console.log(" division ");
+        totalSuma = variables.reduce((primerNumero, segundoNumero) => primerNumero / segundoNumero);
+        document.getElementById("resultado").value = totalSuma;
+    }
+
+    else if (multiplicacion == true){
+        console.log("multiplicacion ");
+        totalSuma = variables.reduce((primerNumero, segundoNumero) => primerNumero * segundoNumero);
+        document.getElementById("resultado").value = totalSuma;
+    }
+
+    procesos.length = 0;
+    variables.length = 0; 
+    operadorUno = parseFloat(document.getElementById("resultado").value);
+    variables.push(operadorUno);
+    operadorUno = 0;
 }
-
+        
+    
 function limpiar(){
     document.getElementById("resultado").value = " ";
+    variables.length = 0; 
+    procesos.length = 0; 
+    totalSuma = 0;
 }
 
