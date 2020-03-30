9d9afe04fd2d754de63a11af26eea55f9556c7db
Silzhar <luis.ruizfer@gmail.com>
2020-03-30 12:45:37 +0200

    Ejercicios por consola / 02

--- javascript/ejercicios-consola/js/ejercicios.js
+++ javascript/ejercicios-consola/js/ejercicios.js
@@ -1,24 +1,50 @@
 // Ejercicio 1
 function ejercicio01(){
     function areaCuadrado(lado){
-        var area = lado * lado;
-        return area
+        var areaC = lado * lado;
+        return areaC
     }
     
-
-    function perimetroCuadrado(area){
-        var perimetro = area * 4
-        return perimetro
+    function perimetroCuadrado(areaC){
+        var perimetroC = areaC * 4
+        return perimetroC
     }
 
     var lado = 20;
     var area1 = areaCuadrado(lado);
-    console.log('Area es igual a : ',area1);
+    console.log('Area de cuadrado es igual a : ',area1);
 
     perimetro1 = perimetroCuadrado(area1);
-    console.log('Perímetro es igual a : ',perimetro1);
+    console.log('Perímetro de cuadrado es igual a : ',perimetro1);
 
 }
 
 ejercicio01();
+
+// Ejercicio 2
+function ejercicio02(){
+    function areaTriangulo(base, altura){
+        var areaT = (base * altura) / 2
+        return areaT
+    }
+
+    function perimetroTriangulo(a, base, c){
+        var perimetroT = a + base + c
+        return perimetroT
+    }
+
+    var base = 12;
+    var altura = 4;
+    area2 = areaTriangulo(base, altura);
+    console.log('Area del triangulo : ',area2);
+
+    var a = 8;
+    var c = 6;
+
+    perimetro2 = perimetroTriangulo(a, base, c);
+    console.log('Perimetro de triangulo :',perimetro2);
+
+}
+
+ejercicio02();
 
