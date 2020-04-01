1c753dc3aa8cd9901f470a0c99057997a99a1529
Silzhar <luis.ruizfer@gmail.com>
2020-04-01 20:22:21 +0200

    ejercicios por consola / de 11 a 15

--- javascript/ejercicios-consola/js/ejercicios.js
+++ javascript/ejercicios-consola/js/ejercicios.js
@@ -1,6 +1,5 @@
 // Ejercicio 1
 console.log('//----------------------/////----------------------//')
-
 function ejercicio01(){
     function areaCuadrado(lado){
         var areaC = lado * lado;
@@ -21,11 +20,8 @@ function ejercicio01(){
 }
 
 ejercicio01();
-
 console.log('//----------------------/////----------------------//')
-
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 2
 function ejercicio02(){
     function areaTriangulo(baseT, altura){
@@ -52,9 +48,7 @@ function ejercicio02(){
 
 ejercicio02();
 console.log('//----------------------/////----------------------//')
-
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 3
 function ejercicio03(){
     function areaRectangulo(baseR, alturaR){
@@ -79,9 +73,7 @@ function ejercicio03(){
 
 ejercicio03();
 console.log('//----------------------/////----------------------//')
-
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 4
 function ejercicio04(){
     function areaParalelogramo(baseP, alturaP){
@@ -106,9 +98,7 @@ function ejercicio04(){
 
 ejercicio04();
 console.log('//----------------------/////----------------------//')
-
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 5 
 function ejercicio05(){
     function areaRombo(diagonal1, diagonal2){
@@ -132,9 +122,7 @@ function ejercicio05(){
 
 ejercicio05();
 console.log('//----------------------/////----------------------//')
-
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 6
 function ejercicio06(){
     function areaCometa(diagonal1, diagonal2){
@@ -161,9 +149,7 @@ function ejercicio06(){
 
 ejercicio06();
 console.log('//----------------------/////----------------------//')
-
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 7
 function ejercicio07(){
     function areaTrapecio(baseMayor, baseInferior, alturaTrap){
@@ -191,9 +177,7 @@ function ejercicio07(){
 
 ejercicio07();
 console.log('//----------------------/////----------------------//')
-
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 8
 function ejercicio08(){
     function perimetroPoligonoRegular(lados, basePR){
@@ -219,8 +203,7 @@ function ejercicio08(){
 
 ejercicio08();
 console.log('//----------------------/////----------------------//')
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 9
 function ejercicio09(){
     function areaCirculo(radio){
@@ -243,8 +226,7 @@ function ejercicio09(){
 
 ejercicio09();
 console.log('//----------------------/////----------------------//')
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 10   Math.pow() para elevar un número a la potencia deseada.
 function ejercicio10(){
     function corona(radioExterior, radioInterior){
@@ -260,8 +242,7 @@ function ejercicio10(){
 
 ejercicio10();
 console.log('//----------------------/////----------------------//')
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
 // Ejercicio 11
 function ejercicio11(){
     function porcionArea(radio, grados){
@@ -277,5 +258,107 @@ function ejercicio11(){
 
 ejercicio11();
 console.log('//----------------------/////----------------------//')
-//------------------------------/////////------------------------------//
-//------------------------------/////////------------------------------//
+//-----------------------------------/////////----------------/////////---------------------------//
+// Ejercicio 12
+function ejercicio12(){
+    function areaCilindro(radio, altura){
+        var areaCil = Math.pow(Math.PI, 2) * radio * (altura + radio);
+        return areaCil
+    }
+
+    function volumenCilindro(radio, altura){
+        var volumen = Math.PI * Math.pow(radio, 2) * altura;
+        return volumen
+    }
+
+    var radio = 6;
+    var altura = 14;
+
+    var area12 = areaCilindro(radio, altura);
+    console.log('Area del cilindro : ',area12.toFixed(2));
+
+    var volumen12 = volumenCilindro(radio, altura);
+    console.log('Volumen del cilindro : ',volumen12.toFixed(2));
+}
+
+ejercicio12();
+console.log('//----------------------/////----------------------//')
+//-----------------------------------/////////----------------/////////---------------------------//
+// Ejercio 13
+function ejercicio13(){
+    function areaCono(radioBase , generatriz){
+        var areaCon = Math.PI * radioBase * (radioBase + generatriz);
+        return areaCon
+    }
+
+    function volumenCono(radioBase, altura){
+        var volumenC = (Math.PI * Math.pow(radioBase, 2) * altura) / 3;
+        return volumenC
+    }
+
+    var radioBase = 18;
+    var generatriz = 32;
+    var altura = 27;
+
+    var area13 = areaCono(radioBase, generatriz);
+    console.log('Area del cono : ',area13.toFixed(2));
+
+    var volumen13 = volumenCono(radioBase, altura);
+    console.log('Volumen del cono : ',volumen13.toFixed(2));
+}   
+
+ejercicio13();
+console.log('//----------------------/////----------------------//')
+//-----------------------------------/////////----------------/////////---------------------------//
+// Ejercio 14
+function ejercicio14(){
+    function areaTroncoCono(radioBaseInferior ,radioBaseSuperior, generatriz){
+        var areatronco = Math.PI * (generatriz * 
+            (radioBaseInferior + radioBaseSuperior) + Math.pow(radioBaseInferior, 2) + Math.pow(radioBaseSuperior, 2));
+        return areatronco
+    }
+
+    function volumenTroncoCono(radioBaseInferior, radioBaseSuperior, alturaTronco){
+        var volumenTC = Math.PI * alturaTronco * 
+        (Math.pow(radioBaseInferior, 2) + Math.pow(radioBaseSuperior, 2) + radioBaseInferior * radioBaseSuperior) / 3;
+        return volumenTC
+    }
+
+    var radioBaseSuperior = 12;
+    var radioBaseInferior = 16
+    var generatriz = 24;
+    var alturaTronco = 29;
+
+    var area14 = areaTroncoCono(radioBaseInferior ,radioBaseSuperior, generatriz);
+    console.log('Area del cono : ',area14.toFixed(2));
+
+    var volumen14 = volumenTroncoCono(radioBaseInferior, radioBaseSuperior, alturaTronco);
+    console.log('Volumen del cono : ',volumen14.toFixed(2));
+}   
+
+ejercicio14();
+console.log('//----------------------/////----------------------//')
+//-----------------------------------/////////----------------/////////---------------------------//
+// Ejercicio 15
+function ejercicio15(){
+    function areaEsfera(radioEsfera){
+        var areaE = 4 * Math.PI * Math.pow(radioEsfera, 2);
+        return areaE
+    }
+
+    function volumenEsfera(radioEsfera){
+        var volumenE = (4 * Math.PI * Math.pow(radioEsfera, 2)) / 3;
+        return volumenE
+    }
+
+    var radioEsfera = 30;
+    var area15 = areaEsfera(radioEsfera);
+    console.log('Area de la esfera :', area15.toFixed(2));
+
+    var volumen15 = volumenEsfera(radioEsfera);
+    console.log('Volumen de la esfera :',volumen15.toFixed(2));
+}
+
+ejercicio15();
+console.log('//----------------------/////----------------------//')
+//-----------------------------------/////////----------------/////////---------------------------//
