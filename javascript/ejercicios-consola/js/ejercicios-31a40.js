a238977bfb6c36ae6425ddedefa925bdee86fbde
Silzhar <luis.ruizfer@gmail.com>
2020-04-07 13:30:18 +0200

    ejercicios por consola / ej 32

--- javascript/ejercicios-consola/js/ejercicios-31a40.js
+++ javascript/ejercicios-consola/js/ejercicios-31a40.js
@@ -1,3 +1,4 @@
+// Ejercicio 31
 function ejercicio31(){
     function n_primos(){  
         for (controller of list_primos){
@@ -17,5 +18,28 @@ function ejercicio31(){
 }
 
 ejercicio31();
+console.log('//----------------------/////----------------------//')
+//-----------------------------------/////////----------------/////////---------------------------//
+// Ejercicio 32
+function ejercicio32(){
+    function nPrimos(){
+        while(contr < contr_max){
+            if(contr % 2 !== 0 & contr % 3 !== 0 & contr % 5 !== 0 & contr % 7 !== 0){
+                list_max.push(contr);
+            }
+            contr++;
+        }
+
+    }
+
+    var contr = 7;
+    var contr_max = 1000;
+    var list_max = [2,3,5,7];
+
+    nPrimos();
+    console.log('Lista de numeros primos : ',list_max);
+}
+
+ejercicio32();
 console.log('//----------------------/////----------------------//')
 //-----------------------------------/////////----------------/////////---------------------------//
