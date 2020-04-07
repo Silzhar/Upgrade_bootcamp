0689ee784d3ca4ddc9e69fdbd6634c7b09a3bf60
Silzhar <luis.ruizfer@gmail.com>
2020-04-07 12:39:45 +0200

    ejercicios por consola / ej 31

---
+++ javascript/ejercicios-consola/js/ejercicios-31a40.js
@@ -0,0 +1,21 @@
+function ejercicio40(){
+    function n_primos(){  
+        for (controller of list_primos){
+            if(controller < control_max){
+                console.log('Encontrado numero primo : ',controller);
+                controller++;
+            }
+        }   
+    }
+
+    var controller = 0;
+    var control_max = 10;
+    var list_primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+        31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89 , 97];
+
+    n_primos();
+}
+
+ejercicio40();
+console.log('//----------------------/////----------------------//')
+//-----------------------------------/////////----------------/////////---------------------------//
