2dcbb7e0c07561b6343a14e2a40321f0151bbbb1
Silzhar <luis.ruizfer@gmail.com>
2020-05-14 17:03:28 +0200

    proyecto angular 2

---
+++ poyectos-angular/angular-sesion-2/angular-project/src/app/goku/gohan/videl/videl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videl',
+  templateUrl: './videl.component.html',
+  styleUrls: ['./videl.component.scss']
+})
+export class VidelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ poyectos-angular/angular-sesion-2/angular-project/src/app/goku/goku.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goku',
+  templateUrl: './goku.component.html',
+  styleUrls: ['./goku.component.scss']
+})
+export class GokuComponent implements OnInit {
+
+  constructor() { }
+
+  // public tecnic: Technique [];
+
+  ngOnInit(): void {
+  }
+
+}
