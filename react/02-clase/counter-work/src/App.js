3b21a8c0412ac1068707ea8dac69155ea10e8fcd
Silzhar <luis.ruizfer@gmail.com>
2020-08-12 18:58:22 +0200

    Repair counter exercise

--- react/02-clase/counter-work/src/App.js
+++ react/02-clase/counter-work/src/App.js
@@ -1,47 +1,19 @@
 import React from 'react';
+
+import Counter from './components/Counter';
+
 import './App.css';
 
 class App extends React.Component {
-
-  state = {
-    counter: 1,
-    value: 1,
-  };
-
-  handleAdd = () => {
-    const newCounter = this.state.counter + this-this.state.value;
-
-    this.setState({
-      counter: newCounter,
-    })
-  }
-
-  handleSubstract = () => {
-    const newCounter = this.state.counter - 1;
-
-    this.setState({
-      counter: newCounter,
-    })
-  }
-  handleChange = (e) => {
-    this.setState({
-      value: Number(e.target.value || 0),
-    })
-  }
-
-  render(){
+  render() {
+  
     return (
       <div className="App">
-        <h2>Contador vale {this.state.counter}</h2>
-        <input type="number" value={this.state.value} onChange={this.handleChange}/>
-        <div>
-          <button onClick={this.handleAdd}>SUMAR</button>
-          <button onClick={this.handleSubstract}>RESTAR</button>
-        </div>
+        <Counter />
+
       </div>
     );
   }
-  
 }
 
 export default App;
